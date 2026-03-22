72139a8f586256ef692bcea33e9a2b59daec62e7
Aniket Maity <aniketmaity600@gmail.com>
2026-03-22 10:04:41 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Onboarding from "./pages/Onboarding"
+import Account from "./pages/Account"
+import Auth from "./pages/Auth"
+import Profile from "./pages/Profile"
+import Navbar from "./components/layout/Navbar"
+import { NeonAuthUIProvider } from "@neondatabase/auth/react"
+import { authClient } from "./lib/auth"
+import AuthProvider from "./context/AuthContext"
+
+
+function App() {
+  
+  return (
+     <NeonAuthUIProvider authClient={authClient} defaultTheme="dark">
+      <AuthProvider>
+     <BrowserRouter>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+
+        <main className="flex-1">
+          <Routes>
+            <Route index element={<Home/>}/>
+            <Route path="/onboarding" element={<Onboarding/>}/>
+            <Route path="/profile" element={<Profile/>}/>
+            <Route path="/account/:pathname" element={<Account/>}/>
+            <Route path="/auth/:pathname" element={<Auth/>}/>
+          </Routes>
+        </main>
+
+      </div>
+    </BrowserRouter>
+    </AuthProvider>
+    </NeonAuthUIProvider>
+  )
+}
+
+export default App
