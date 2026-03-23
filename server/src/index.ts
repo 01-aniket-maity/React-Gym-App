9a6dcdedaf22edfcdbd822583b8d24e3a008f5cf
Aniket Maity <142742057+01-aniket-maity@users.noreply.github.com>
2026-03-23 09:59:26 +0530

    Modify CORS configuration for specific origin
    Update CORS settings to allow requests from specific origin.

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
+app.use(cors({
+  origin: "https://react-gym-app-beta.vercel.app",
+  credentials: true
+}));
 app.use(cookieParser());
 app.use(express.json());
 
